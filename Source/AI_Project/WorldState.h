3c83f6f944a5113e8bb6ef950a2f2f42664f6457
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2026-03-13 23:46:39 +0000

    new plan/ experiment

--- Source/AI_Project/WorldState.h
+++ Source/AI_Project/WorldState.h
@@ -11,12 +11,53 @@
  */
 
 
+UENUM(BlueprintType)
+enum class EWorldValueType : uint8
+{
+	INT,
+	BOOL, 
+	FLOAT,
+	VECTOR,
+	ACTOR
+};
+
+
+USTRUCT(BlueprintType)
+struct FWorldValue
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EWorldValueType ValueType;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta =(EditCondition = "ValueType == EWorldValueType::INT"))
+	int32 IntValue;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta =(EditCondition = "ValueType == EWorldValueType::BOOL"))
+	bool bValue;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta =(EditCondition = "ValueType == EWorldValueType::FLOAT"))
+	float FloatValue;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta =(EditCondition = "ValueType == EWorldValueType::VECTOR"))
+	FVector VectorValue;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta =(EditCondition = "ValueType == EWorldValueType::ACTOR"))
+	AActor* ActorValue;
+	
+};
+
+
 USTRUCT(BlueprintType)
 struct FWorldState
 {
 	GENERATED_BODY()
 	
 	public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FWorldValue test; 
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TMap<FString, bool> StateValues;
 	
