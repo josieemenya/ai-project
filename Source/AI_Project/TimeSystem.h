766bc2dd6aa752435692c052ebe0a6023e509bd6
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2026-04-16 22:56:40 +0100

    got somrwhere ish, just a matter of having getters and setters

---
+++ Source/AI_Project/TimeSystem.h
@@ -0,0 +1,122 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/TimelineComponent.h"
+#include "UObject/NoExportTypes.h"
+#include "TimeSystem.generated.h"
+
+/**
+ * 
+ */
+
+class UTimelineComponent;
+
+UENUM(BlueprintType)
+enum class ETimeOfDay : uint8
+{
+	MORNING, 
+	EVENING, 
+	AFTERNOON, 
+	TWILIGHT
+};
+
+USTRUCT(BlueprintType)
+struct FTimeRange
+{
+	
+	GENERATED_BODY()
+	public:
+	
+	UPROPERTY(BlueprintReadWrite)
+	int32 StartRange; 
+	
+	UPROPERTY(BlueprintReadWrite)
+	int32 EndRange;
+	
+};
+
+USTRUCT(BlueprintType)
+struct FTimeData
+{
+	GENERATED_BODY()
+	public:
+	
+	TArray<TPair<FTimeRange, ETimeOfDay>> Set;
+	
+	UPROPERTY(BlueprintReadWrite)
+	int32 Hour; 
+	
+	UPROPERTY(BlueprintReadWrite)
+	int32 Minute;
+	
+	UPROPERTY(BlueprintReadWrite)
+	int32 Day; 
+	
+	
+	
+};
+ 
+UCLASS(Blueprintable)
+class AI_PROJECT_API UTimeSystem : public UActorComponent
+{
+	GENERATED_BODY()
+public:
+	
+	UTimeSystem(); 
+	
+	ETimeOfDay CurrentTimeOfDay;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FTimeData TimeData;
+	
+	UFUNCTION(BlueprintCallable)
+	void TransitionTimeOfDay(ETimeOfDay e);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UTimelineComponent> TimelineClass;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UTimelineComponent* Timeline;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UCurveFloat* TimeCurve;
+	
+	FOnTimelineFloat OnTimeline;
+	
+	UFUNCTION(BlueprintCallable)
+	void StartDay(); 
+	
+	UFUNCTION(BlueprintCallable)
+	void UpdateTime(); 
+	
+	UFUNCTION(BlueprintCallable)
+	void OnTimelineUpdate(float val); 
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CurrentPosition; 
+	
+	UFUNCTION(BlueprintCallable)
+	void InitTimeline();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<FString, float> DayPositions;
+	
+	TArray<FString> Times;
+	float DesiredPosition;
+	
+	class ADirectionalLight* LightActor; 
+	
+	float TotalSecondsElapsed;
+	
+	float StartPosition;
+	
+	float LastDesiredPosition;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction) override;
+	
+};
